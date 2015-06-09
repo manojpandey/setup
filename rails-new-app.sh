87ff66364a820589b4df3f4f70819806b2df8ccf
Manoj Pandey <manojpandey1996@gmail.com>
2015-06-10 04:03:46 +0530

    ror new app

---
+++ rails-new-app.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Rails simple App
+#### If you want to use SQLite (not recommended)
+rails new myapp
+
+#### If you want to use MySQL
+rails new myapp -d mysql
+
+#### If you want to use Postgres
+# Note that this will expect a postgres user with the same username
+# as your app, you may need to edit config/database.yml to match the
+# user you created earlier
+rails new myapp -d postgresql
+
+# Move into the application directory
+cd myapp
+
+# If you setup MySQL or Postgres with a username/password, modify the
+# config/database.yml file to contain the username/password that you specified
+
+# Create the database
+rake db:create
+
+rails server
+
+#You can now visit http://localhost:3000 to view your new website!
